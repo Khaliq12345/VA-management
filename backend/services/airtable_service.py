655b8c0565aee302542ef62d4bbb347db55ded5c
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-04-24 23:05:34 +0100

    Airtable service updated

--- backend/services/airtable_service.py
+++ backend/services/airtable_service.py
@@ -1,23 +1,19 @@
-from dotenv import load_dotenv
+from backend.config import config
 import httpx
-import os
 
-load_dotenv()
 
 AIRTABLE_API_URL = "https://api.airtable.com/v0"
-AIRTABLE_PERSONAL_ACCESS_TOKEN = os.getenv("AIRTABLE_PERSONAL_ACCESS_TOKEN")
 
 HEADERS = {
-    "Authorization": f"Bearer {AIRTABLE_PERSONAL_ACCESS_TOKEN}",
+    "Authorization": f"Bearer {config.AIRTABLE_PERSONAL_ACCESS_TOKEN}",
     "Content-Type": "application/json",
 }
 
 
-async def get_creator_info_from_airtable(table_id, base_id, limit=20, offset=None):
+async def get_creator_info_from_airtable(table_id, base_id, limit=20, offset=0):
     url = f"{AIRTABLE_API_URL}/{base_id}/{table_id}"
     params = {"pageSize": limit}
-    if offset:
-        params["offset"] = offset
+    params["offset"] = offset
 
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=HEADERS, params=params)
@@ -28,7 +24,8 @@ async def get_creator_info_from_airtable(table_id, base_id, limit=20, offset=Non
     instagram_usernames = []
     for record in records:
         fields = record.get("fields", {})
-        creators_usernames.append(fields.get("Creator Name"))
-        instagram_usernames.append(fields.get("Instagram Username"))
+        if fields.get("Creator Name"):
+            creators_usernames.append(fields.get("Creator Name"))
+            instagram_usernames.append(fields.get("Instagram Username"))
 
     return instagram_usernames, creators_usernames
