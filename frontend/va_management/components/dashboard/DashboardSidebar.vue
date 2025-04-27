eefc309c5bb4aeaa2c07a0d7a283b754d2310766
Hafiz <abdulmoustapha64@@gmail.com>
2025-04-27 19:30:25 +0100

    Added APIs in Front

--- frontend/va_management/components/dashboard/DashboardSidebar.vue
+++ frontend/va_management/components/dashboard/DashboardSidebar.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-
 const props = defineProps({
   creators: {
-      type: Object,
-    },
+    type: Object,
+  },
 
-    activeCreator: {
-      type: Object,
-      default:{}
-    },
-  });
+  activeCreator: {
+    type: Object,
+    default: {}
+  },
+});
 
 const emit = defineEmits(['menu-item-clicked', 'close-sidebar']);
 
@@ -23,25 +22,22 @@ const isMobile = computed(() => {
   <div :class="{ 'py-4 pl-4 flex flex-col': isMobile, 'bg-gray-200 p-4 flex flex-col': !isMobile }">
     <div v-if="!isMobile" class="flex justify-center mb-4">
       <h3 class="ml-3 text-xl font-bold text-center ">DashBoard</h3>
-        </div>
-    <div  :class="{ 'items-center': isMobile }">
+    </div>
+    <div :class="{ 'items-center': isMobile }">
       <h3 class="ml-3 text-xl font-semibold text-center ">Creators List</h3>
     </div>
     <USeparator class="my-4 " color="neutral" size="md" />
     <nav class="flex-1 space-y-4">
-
-      <UButton v-for="ceator in props.creators"   variant="ghost"   class="text-black hover:bg-gray-700 hover:text-white flex items-center py-2  rounded-lg w-full"
-      :class="{ 'bg-gray-800 text-white': ceator.id == activeCreator.id }"   
-      @click="$emit('menu-item-clicked', ceator)">
+      <UButton v-for="ceator in props.creators" variant="ghost"
+        class="text-black hover:bg-gray-700 hover:text-white flex items-center py-2  rounded-lg w-full"
+        :class="{ 'bg-gray-800 text-white': ceator['Model Assigned'] == activeCreator['Model Assigned'] }"
+        @click="$emit('menu-item-clicked', ceator)">
         <UIcon name="i-heroicons-users-20-solid" class="mr-2 w-5 h-5" />
-        <span class="ml-1">{{ceator.name}}</span>
-        <UBadge class="bg-error-100 rounded-full" :class="{'bg-primary-100': ceator.strategy == 'F/U', 'bg-secondary-100': ceator.strategy == 'M-Comment'} " >{{ceator.strategy}}</UBadge>
+        <span class="ml-1">{{ ceator['Model Assigned'] }}</span>
+        <UBadge class="bg-error-100 rounded-full"
+          :class="{ 'bg-primary-100': ceator.Strategy == 'F/U', 'bg-secondary-100': ceator.Strategy == 'Mass Comment' }">
+          {{ ceator.Strategy }}</UBadge>
       </UButton>
-     
-      
     </nav>
-
   </div>
 </template>
-
-
--- frontend/va_management/components/dashboard/UserCard.vue
+++ frontend/va_management/components/dashboard/UserCard.vue
@@ -1,7 +1,5 @@
 
 <script setup lang="ts">
-import { computed } from 'vue';
-
 const props = defineProps({
   user: {
     type: Object,
@@ -10,16 +8,21 @@ const props = defineProps({
 });
 
 function statusColor(status: string) {
-  switch (status) {
-    case 'active': return 'bg-green-500'
-    case 'pending': return 'bg-yellow-400'
-    case 'inactive': return 'bg-red-500'
-    default: return 'bg-gray-400'
-  }
+  return 'bg-gray-800';
+  // switch (status) {
+  //   case 'not active': return 'bg-green-500'
+  //   case 'active': return 'bg-red-500'
+  //   default: return 'bg-gray-400'
+  // }
 }
 
-function goToDetail(id: number) {
-  useRouter().push(`{ path: '/user-details', query: { id: user.id } }${id}`)
+function goToUserDetails() {
+  console.log("user", props.user)
+  navigateTo({
+    path: '/user-details',
+    query: { id: props.user.scraped_user.username }, // Optionnel : pour l'URL
+    state: { user: props.user } // Transmet l'objet complet
+  });
 }
 
 </script>
@@ -27,14 +30,14 @@ function goToDetail(id: number) {
 
 <template>
   <div class="my-4 text-start">
-    <div :key="user.id" class="flex bg-white rounded shadow">
-      <div :class="['w-1 rounded-l', statusColor(user.status)]"></div>
+    <div :key="user.scraped_user.id" class="flex bg-white rounded shadow">
+      <div :class="['w-1 rounded-l', statusColor(user.scraped_user.status)]"></div>
       <div class="pl-4 flex-1">
-        <h3 class="text-lg font-semibold">{{ user.name }}</h3>
-        <p class="text-sm text-gray-500">{{ user.email }}</p>
-        <p class="text-sm text-gray-400">Last Action : {{ new Date(user.lastLogin).toLocaleDateString() }}</p>
+        <h3 class="text-lg font-semibold">{{ user.scraped_user.username }}</h3>
+        <p class="text-sm text-gray-500">Created At : {{ user.scraped_user.created_at }}</p>
+        <p class="text-sm text-gray-400">Last Action : {{ user.scraped_user.last_action ?? " - - - " }}</p>
         <div class="flex justify-end">
-          <NuxtLink :to="{ path: '/user-details', query: { id: user.id } }" class="btn">
+          <NuxtLink :to="{ path: '/user-details', query: { id: user.scraped_user.username, user: JSON.stringify(user)  }}" class="btn">
             <UButton variant="link" class="mt-2 cursor-pointer" >Show Details</UButton>
           </NuxtLink>
         </div>
