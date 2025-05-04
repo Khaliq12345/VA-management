7ec89e90c27a7abdc34ba4031fa42bb11d2b74ff
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-05-05 00:37:56 +0100

    Docker added for deployment

--- backendApp/backend/main.py
+++ backendApp/backend/main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "__main__:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True if config.APP_ENV == "development" else False,
     )
